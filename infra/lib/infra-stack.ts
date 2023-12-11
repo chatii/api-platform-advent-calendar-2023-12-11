81ca1039ba1fbf77780a7505750d8d63b4c058a1
chatii <chatii@inbaa.dev>
2023-12-11 13:03:25 +0900

    LambdaのFunctional URLsでレスポンスが返ってくるところまで

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,16 +1,29 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {Construct} from 'constructs';
+import {PhpFpmFunction} from "@bref.sh/constructs";
+import {AssetCode, FunctionUrlAuthType, Tracing} from "aws-cdk-lib/aws-lambda";
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const helloApi = new PhpFpmFunction(this, 'Hello', {
+      handler: 'public/index.php',
+      phpVersion: "8.3",
+      code: new AssetCode("../api/", {
+        exclude: [
+            'tests/**',
+            'var/**',
+        ],
+      }),
+      tracing: Tracing.ACTIVE,
+      environment: {
+        'APP_ENV': 'prod',
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    helloApi.addFunctionUrl({
+      authType: FunctionUrlAuthType.NONE,
+    });
   }
 }
